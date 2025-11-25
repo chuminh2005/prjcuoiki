ef3731db16cc3100eaf47b3ebeefc50d7278724a
chuminh2005 <cdminh410@gmail.com>
2025-11-25 10:47:07 +0700

    update cu

--- connect.php
+++ connect.php
@@ -1,5 +1,8 @@
 <?php
-
+$host = 'vn1.loadip.com';
+$dbname = 'coll5txb_projectcnw';
+$username = 'coll5txb_minh';
+$password = 'Minh04102005';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
